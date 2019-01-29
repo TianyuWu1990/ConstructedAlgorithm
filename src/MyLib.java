1d38f2e6f44483a3cce98cf555791a52306e3536
Tianyu Wu <wty773752792@gmail.com>
2019-01-28 21:55:09 -0500

    Update MyLib.java

--- src/MyLib.java
+++ src/MyLib.java
@@ -24,20 +24,39 @@ class MyLib {
 		}
 	}
   
-  public int search(int[] nums, int target) {
+  public int binarySearch(int[] nums, int target) {
         int low = 0, hi = nums.length - 1;
         while (low <= hi) {
             int mid = low + (hi - low) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
-                hi = mid - 1;
+                hi = mid - 1; //hi = mid WRONG!
             } else {
                 low = mid + 1;
             }
         }
         return -1;
     }
-	
-	
-}
+  public boolean twoDBinarySearch(int[][] matrix, int target) { 
+		if (matrix.length == 0 || matrix[0].length == 0) 
+			return false; 
+		int row = matrix.length; 
+		int col = matrix[0].length; 
+		int i = 0;// left 
+		int j = row * col - 1;// right 
+		while (i <= j) { 
+			int mid = i + (j - i)/2; 
+			int r = mid / col; // helper function to map n-dimensional 
+			// coordinate to 1D coordinate (vice versa) 
+			int c = mid % col; 
+			if (matrix[r][c] == target) 
+				return true; 
+			else if (matrix[r][c] > target) 
+				j = mid - 1; 
+			else 
+				i = mid + 1; 
+		} 
+		return false; 
+		}
+	}
