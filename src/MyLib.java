d3e86eacc9553887d5a4d1f6ee190d55f36dd279
Tianyu Wu <wty773752792@gmail.com>
2019-01-28 21:41:16 -0500

    Update MyLib.java

--- src/MyLib.java
+++ src/MyLib.java
@@ -24,5 +24,20 @@ class MyLib {
 		}
 	}
   
-  
+  public int search(int[] nums, int target) {
+        int low = 0, hi = nums.length - 1;
+        while (low <= hi) {
+            int mid = low + (hi - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                hi = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+	
+	
 }
