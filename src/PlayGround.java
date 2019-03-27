e80afc59f9db51b5583cb48a2c79675cf0e1b9b6
TianyuWu1990 <wty773752792@gmail.com>
2019-03-27 16:17:25 -0400

    bfs word ladder I + II

--- src/PlayGround.java
+++ src/PlayGround.java
@@ -7,12 +7,23 @@ import java.util.*;
 
 class PlayGround {
     public static void main(String[] args) {
-        List<Integer> list1 = Arrays.asList(1,2,3);
-        ArrayList<Integer> list = new ArrayList<>(list1);
-        System.out.println(list.get(1));
-        list.add(4);
-        System.out.println(list.get(1));
-        Integer.val
+//        List<Integer> list1 = Arrays.asList(1,2,3);
+//        ArrayList<Integer> list = new ArrayList<>(list1);
+//        ListNode n1 = new ListNode(1);
+//        ListNode n2 = new ListNode(2);
+//        ListNode n3 = new ListNode(3);
+//        n1.next = n2;
+//        System.out.println(n1.next.value);
+//        ListNode new_node  = n1;
+//        n1.next.next = n3;
+//        System.out.println(new_node.next.next.value);
+//
+        System.out.println(test());
+    }
 
+    private static int test() {
+        int i = 1;
+        int a = 3;
+        return i = a;
     }
 }
