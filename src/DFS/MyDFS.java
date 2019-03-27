4c4a28cb199f9236124830969edec625081ccb6b
TianyuWu1990 <wty773752792@gmail.com>
2019-03-27 16:04:03 -0400

    word ladder II

--- src/DFS/MyDFS.java
+++ src/DFS/MyDFS.java
@@ -17,4 +17,93 @@ public class MyDFS {
         if(!valid) invalid[i][j] = true;
         return valid;
     }
+
+    /**
+     * Word Ladder II
+     */
+    // final result
+    List<List<String>> res = new ArrayList<>();
+    // temp list for backtrack
+    List<String> list = new LinkedList<>();
+    // <mutation: list<father>>
+    Map<String, List<String>> map = new HashMap<>();
+
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        if (wordList.size() == 0) return res;
+        // BFS each mutation
+        Queue<String> q = new ArrayDeque<>();
+        // temp visited
+        Set<String> visited = new HashSet<>();
+        // unmutable unvisited
+        Set<String> unvisited = new HashSet<>(wordList);
+        q.add(beginWord);
+        // remove the begin word first
+        unvisited.remove(beginWord);
+        boolean found = false;
+
+        // bfs
+        while(!q.isEmpty()) {
+            int size = q.size();
+            //current level
+            for (int k = size - 1; k >= 0; k--) { // for each string in the queue
+                String word = q.poll();
+                for (int i = 0; i < word.length(); i++) {
+                    // change one char a time then make new string to compare
+                    char chs[] = word.toCharArray();
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        chs[i] = c;
+                        String newStr = new String(chs);
+                        // if mutation is unvisted
+                        if (unvisited.contains(newStr)) {
+                            // update the visited and add to queue for next validation
+                            if (!visited.contains(newStr)) {
+                                visited.add(newStr);
+                                q.add(newStr);
+                            }
+                            // build adjacent graph
+                            //<mutation, fathers>
+                            if (map.containsKey(newStr)) map.get(newStr).add(word);
+                            else {
+                                List<String> l = new ArrayList<>();
+                                l.add(word);
+                                map.put(newStr, l);
+                            }
+                            if (newStr.equals(endWord)) found = true;
+                        }
+                    }//a-z
+                }//first index-last index
+            }//for each string
+            if (found) break;
+            // unvisted is unmutable
+            unvisited.removeAll(visited);
+            // queue's visited need to rebuild
+            visited.clear();
+
+        }
+
+        // back trace based on the adjacent graph that we have built
+        backTrace(endWord, beginWord);
+        return res;
+    }
+
+    private void backTrace(String cur, String start) {
+        if (cur.equals(start)) {
+            list.add(0, start);
+            res.add(new ArrayList<String>(list));
+            list.remove(0); // backtrace by one step
+            return;
+        }
+        list.add(0, cur);
+        if (map.get(cur) != null) {
+            for (String s:map.get(cur)) { // for each neighbors
+                //set cur to its every father
+                backTrace(s,start);
+            }
+        }
+        list.remove(0);
+    }
+}
+
+
+
 }
